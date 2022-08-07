c85eded697685befa4b1d6523b71569a0a5586f7
Cayean <collinayala@gmail.com>
2022-08-07 00:11:09 -0500

    added pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// user is introduced to the quiz
+
+// user clicks on the start quiz button
+
+// the button creates a timer on the top of the screen
+
+//The instructions and the Start quiz button get removed
+
+// the user is then shown the first question with button responses
+
+// if the user selects an answer
+
+// the quiz determines if the answer is correct or not
+
+// the if the answer is correct the user proceeds to the next question
+
+// The user will gain points for the correct answers
+
+// if the user is not correct a message will be discplayed that it was incorrect
+
+// The user will lose points
+
+
+
